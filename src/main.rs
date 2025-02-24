fc58e6267518c54a3499168763a3f268c48a909e
Anton Pavliuk <streetreadger@gmail.com>
2025-02-24 23:10:04 +0200

    Add example of osascript witch I can use

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use cacao::appkit::{App, AppDelegate};
+use cacao::appkit::window::Window;
+
+#[derive(Default)]
+struct BasicApp {
+    window: Window
+}
+
+impl AppDelegate for BasicApp {
+    fn did_finish_launching(&self) {
+        self.window.set_minimum_content_size(400., 400.);
+        self.window.set_title("Hello world!");
+        self.window.show();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let t =
+        r#"tell application "System Events" tell every desktop
+        set picture to "imagePath"
+        end tell
+        end tell"#;
+    App::new("com.hello.world", BasicApp::default()).run();
 }
